406a81ecd39076ac12d1597c92c25e74ba8c25bc
Nikhil ER <nikhilravi9847@gmail.com>
2023-06-22 12:12:27 +0530

    First commit Git Test

---
+++ src/test/java/Syn_Maven/Maven_Test/RestAssured_Test.java
@@ -0,0 +1,12 @@
+package Syn_Maven.Maven_Test;
+
+import org.testng.annotations.Test;
+
+public class RestAssured_Test {
+
+	@Test
+	public void rest()
+	{
+		System.out.println("Rest assured");
+	}
+}
---
+++ src/test/java/Syn_Maven/Maven_Test/Selenium_Test.java
@@ -0,0 +1,13 @@
+package Syn_Maven.Maven_Test;
+
+import org.testng.annotations.Test;
+
+public class Selenium_Test {
+	
+	@Test
+	public void selenium()
+	{
+		System.out.println("Selenium test");
+	}
+
+}
